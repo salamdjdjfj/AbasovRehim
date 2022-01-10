3d04c96c7ace6e5ef79af0a6cd7caf45eae86745
Hesenovhuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2022-01-10 16:38:54 +0400

    update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -66,7 +66,7 @@ async def start_private(client: Client, message: Message):
                         url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
                     )
                 ],
-                [InlineKeyboardButton("❓ Əsas bələdçi",❓ callback_data="cbhowtouse")],
+                [InlineKeyboardButton("❓ Əsas bələdçi", callback_data="cbhowtouse")],
                 [
                     InlineKeyboardButton("📚 Əmrlər", callback_data="cbcmds"),
                     InlineKeyboardButton("❤️ Sahib", url=f"https://t.me/{OWNER_NAME}"),
@@ -129,7 +129,7 @@ async def help(client: Client, message: Message):
 
 ⚡ __Powered by {BOT_NAME} A.I__""",
         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton(text="❓ Əsas bələdçi",❓   callback_data="cbguide")]]
+            [[InlineKeyboardButton(text="❓ Əsas bələdçi",  callback_data="cbguide")]]
         ),
     )
 
