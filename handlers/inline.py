300d06ce107f837359eb5ba2908f5985d3db75d1
Hesenovhuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2022-01-11 14:26:07 +0400

    update inline.py

--- handlers/inline.py
+++ handlers/inline.py
@@ -17,7 +17,7 @@ async def inline(client: Client, query: InlineQuery):
         await client.answer_inline_query(
             query.id,
             results=answers,
-            switch_pm_text="type a youtube video name...",
+            switch_pm_text="youtube video adı yazın...",
             switch_pm_parameter="help",
             cache_time=0,
         )
@@ -44,6 +44,6 @@ async def inline(client: Client, query: InlineQuery):
             await query.answer(
                 results=answers,
                 cache_time=0,
-                switch_pm_text="Error: search timed out",
+                switch_pm_text="Xəta: axtarış vaxtı bitdi",
                 switch_pm_parameter="",
             )
