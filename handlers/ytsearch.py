ad9a2cbb85b5542216a7ee1bdec2b93acbcff42a
Hesenovhuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2022-01-11 17:01:07 +0400

    update ytsearch.py

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -26,7 +26,7 @@ async def ytsearch(_, message: Message):
         [
             [
                 InlineKeyboardButton(
-                    "🗑 Close", callback_data="close",
+                    "🗑 Bağla", callback_data="close",
                 )
             ]
         ]
@@ -34,18 +34,18 @@ async def ytsearch(_, message: Message):
     
     try:
         if len(message.command) < 2:
-            await message.reply_text("/search **needs an argument !**")
+            await message.reply_text("/search **arqument lazımdır !**")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("🔎 **Searching...**")
+        m = await message.reply_text("🔎 **Axtarılır...**")
         results = YoutubeSearch(query, max_results=5).to_dict()
         i = 0
         text = ""
         while i < 5:
-            text += f"🏷 **Name:** __{results[i]['title']}__\n"
-            text += f"⏱ **Duration:** `{results[i]['duration']}`\n"
-            text += f"👀 **Views:** `{results[i]['views']}`\n"
-            text += f"📣 **Channel:** {results[i]['channel']}\n"
+            text += f"🏷 **Ad:** __{results[i]['title']}__\n"
+            text += f"⏱ **Müddəti** `{results[i]['duration']}`\n"
+            text += f"👀 **Baxışlar:** `{results[i]['views']}`\n"
+            text += f"📣 **Kanal:** {results[i]['channel']}\n"
             text += f"🔗: https://www.youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
         await m.edit(text, reply_markup=keyboard, disable_web_page_preview=True)
